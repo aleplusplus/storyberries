6fb94fbb1ebe9036176e9962390246668893d768
Alejandro Labrada Diaz <alabradadiaz@gmail.com>
2018-01-10 13:05:49 -0500

    Add AppId

---
+++ lambda/custom/audioAssets.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [
+    {
+        'title' : 'Tilly’s Rainbow Shoes 0',
+        'url' : 'https://s3.amazonaws.com/storyberries/tilly-rainbow-shoes/speech0.mp3'
+    },
+    {
+        'title' : 'Tilly’s Rainbow Shoes 1',
+        'url' : 'https://s3.amazonaws.com/storyberries/tilly-rainbow-shoes/speech1.mp3'
+    },
+    {
+        'title' : 'Tilly’s Rainbow Shoes 2',
+        'url' : 'https://s3.amazonaws.com/storyberries/tilly-rainbow-shoes/speech2.mp3'
+    },
+    {
+        'title' : 'Tilly’s Rainbow Shoes 3',
+        'url' : 'https://s3.amazonaws.com/storyberries/tilly-rainbow-shoes/speech3.mp3'
+    },
+    {
+        'title' : 'Tilly’s Rainbow Shoes 4',
+        'url' : 'https://s3.amazonaws.com/storyberries/tilly-rainbow-shoes/speech4.mp3'
+    },
+    {
+        'title' : 'Tilly’s Rainbow Shoes 5',
+        'url' : 'https://s3.amazonaws.com/storyberries/tilly-rainbow-shoes/speech5.mp3'
+    },
+];
+
+module.exports = audioData;
